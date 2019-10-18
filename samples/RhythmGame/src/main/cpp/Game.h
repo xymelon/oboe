d93844b6ce3604457468cfb1585361e0548a13b6
Don Turner <donturner@google.com>
2019-10-18 13:58:49 +0100

    Set starting state for codelab

--- samples/RhythmGame/src/main/cpp/Game.h
+++ samples/RhythmGame/src/main/cpp/Game.h
@@ -40,7 +40,7 @@ enum class GameState {
     FailedToLoad
 };
 
-class Game : public AudioStreamCallback {
+class Game {
 public:
     explicit Game(AAssetManager&);
     void start();
@@ -51,33 +51,10 @@ public:
     void tick();
     void tap(int64_t eventTimeAsUptime);
 
-    // Inherited from oboe::AudioStreamCallback
-    DataCallbackResult
-    onAudioReady(AudioStream *oboeStream, void *audioData, int32_t numFrames) override;
-    void onErrorAfterClose(AudioStream *oboeStream, Result error) override;
-
 private:
     AAssetManager& mAssetManager;
-    AudioStream *mAudioStream { nullptr };
-    std::unique_ptr<Player> mClap;
-    std::unique_ptr<Player> mBackingTrack;
-    Mixer mMixer;
-    std::unique_ptr<float[]> mConversionBuffer { nullptr }; // For float->int16 conversion
-
-    LockFreeQueue<int64_t, kMaxQueueItems> mClapEvents;
-    std::atomic<int64_t> mCurrentFrame { 0 };
-    std::atomic<int64_t> mSongPositionMs { 0 };
-    LockFreeQueue<int64_t, kMaxQueueItems> mClapWindows;
-    LockFreeQueue<TapResult, kMaxQueueItems> mUiEvents;
-    std::atomic<int64_t> mLastUpdateTime { 0 };
-    std::atomic<GameState> mGameState { GameState::Loading };
-    std::future<void> mLoadingResult;
 
     void load();
-    TapResult getTapResult(int64_t tapTimeInMillis, int64_t tapWindowInMillis);
-    bool openStream();
-    bool setupAudioSources();
-    void scheduleSongEvents();
 };
 
 
