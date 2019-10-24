03953bd9c08863a6f1b5ae8e2b930a7391d85652
Don Turner <donturner@google.com>
2019-10-24 10:20:35 -0700

    Braced initialization

--- samples/RhythmGame/src/main/cpp/Game.h
+++ samples/RhythmGame/src/main/cpp/Game.h
@@ -53,7 +53,7 @@ public:
 
 private:
     AAssetManager& mAssetManager;
-    GameState mGameState = GameState::Loading;
+    GameState mGameState { GameState::Loading };
 
     void load();
     TapResult getTapResult(int64_t tapTimeInMillis, int64_t tapWindowInMillis);
